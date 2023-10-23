df480e4934ce0cdf8043c1e4c9632f1285d3ed7d
Caro Valverde <carolina.valverde255@gmail.com>
2023-10-23 18:34:52 -0300

    agregamos como declarar variables y mostrar mensajes

---
+++ js/01-variables.js
@@ -0,0 +1,13 @@
+// esto es un comentario de una linea
+
+/* este es un comentario 
+ de multiples lineas */
+
+// declarar una variable (var - let - const)
+
+let anioActual = 2023;
+
+// mostrar mensajes
+console.log(anioActual);
+console.log('anioActual');
+console.log("Año Actual: " + anioActual);
