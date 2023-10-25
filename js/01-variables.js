dee0e3acbf138d9c035783ad15c9bdca125d40ae
Caro Valverde <carolina.valverde255@gmail.com>
2023-10-25 17:20:54 -0300

    agregamos un document write y un alert

--- js/01-variables.js
+++ js/01-variables.js
@@ -6,8 +6,22 @@
 // declarar una variable (var - let - const)
 
 let anioActual = 2023;
+const universe = "https://universe.rollingcodeschool.com/"
 
 // mostrar mensajes
 console.log(anioActual);
 console.log('anioActual');
 console.log("Año Actual: " + anioActual);
+console.log(universe);
+
+// modificar el contenido de una variable
+anioActual = 2024;
+console.log(anioActual);
+
+document.write('<p class="parrafoDestacado" >esto es una prueba de document.write</p>')
+
+// ventanas emergentes
+alert('Hola Mundo');
+
+
+
