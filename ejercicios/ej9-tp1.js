0887d49a0730a48626fd48840c9074c7a7cbf574
Caro Valverde <carolina.valverde255@gmail.com>
2023-10-30 18:47:39 -0300

    agregamos ej9 incompleto

---
+++ ejercicios/ej9-tp1.js
@@ -0,0 +1,12 @@
+// 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+// Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+// Ejemplo:
+// input: Hola mundo
+// Output: oauo
+
+const frase = prompt ('Ingrese una frase');
+//hola mundo
+console.log(frase.length);
+console.log(frase.charAt(0));
+
+// bucle for mas charat
