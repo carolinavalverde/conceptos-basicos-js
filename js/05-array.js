4d3c4b8897d00b7177c8481b97ba3dd7fb630452
Caro Valverde <carolina.valverde255@gmail.com>
2023-11-08 17:51:49 -0300

    agregamos metodos para eliminar elementos del array

--- js/05-array.js
+++ js/05-array.js
@@ -54,3 +54,31 @@ for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas
 }
 document.write(`</ul>`);
 
+//borrar el primer elemento del array
+peliculas.shift();
+document.write(`<h2>Lista de Películas, eliminamos el primer elemento del array (${peliculas.length})</h2>`);
+document.write(`<ul>`);
+for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++){
+    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
+}
+document.write(`</ul>`);
+
+//borrar elementos del medio del array
+peliculas.splice(6, 1);//agrego elemento en medio del array
+//peliculas.splice(6, 2);//borro dos elementos desde la posición q especifico hasta el segun nr, dos numeros 
+//peliculas.splice(6);//borra desde la posición 6 en adelante
+document.write(`<h2>Lista de Películas, eliminamos el elemento de la posicion 6 (${peliculas.length})</h2>`);
+document.write(`<ul>`);
+for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++){
+    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
+}
+document.write(`</ul>`);
+
+//eliminar el ultimo elemento del array
+peliculas.pop();
+document.write(`<h2>Lista de Películas, eliminamos el ultimo elemento del array (${peliculas.length})</h2>`);
+document.write(`<ul>`);
+for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++){
+    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
+}
+document.write(`</ul>`);
