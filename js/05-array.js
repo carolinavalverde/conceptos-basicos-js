0b5261cacd08e889649b8abda51bbd11ea37576e
Caro Valverde <carolina.valverde255@gmail.com>
2023-11-08 17:20:28 -0300

    agregamo smetodos para insertar elementos en array de peliculas

--- js/05-array.js
+++ js/05-array.js
@@ -18,3 +18,30 @@ for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas
     document.write(`<li>${peliculas[indicePeliculas]}</li>`);
 }
 document.write(`</ul>`);
+
+//agregar elementos al array
+peliculas.unshift("Batman","Ironman 1");
+document.write(`<h2>Lista de Películas, con dos pelis nuevas (${peliculas.length})</h2>`);
+document.write(`<ul>`);
+for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++){
+    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
+}
+document.write(`</ul>`);
+
+peliculas.splice(3, 0, "Scream");//agrego elemento en medio del array
+document.write(`<h2>Lista de Películas, agregamos la peli Scream (${peliculas.length})</h2>`);
+document.write(`<ul>`);
+for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++){
+    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
+}
+document.write(`</ul>`);
+
+//agrego elemento al final del array, push
+peliculas.push("Gran Turismo");
+document.write(`<h2>Lista de Películas, agregamos la peli Gran Turismo (${peliculas.length})</h2>`);
+document.write(`<ul>`);
+for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++){
+    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
+}
+document.write(`</ul>`);
+
