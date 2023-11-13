4212833d8f7879505f3afd600e242412ea072afe
Caro Valverde <carolina.valverde255@gmail.com>
2023-11-13 17:17:00 -0300

    agrego ejemplo de arrow function

--- js/06-funciones.js
+++ js/06-funciones.js
@@ -1,31 +1,43 @@
 //funciones declarativas
-function saludar(){
-    //todo el codigo que sabe hacer la funcion
-    document.write(`<p>Hola Mundo</p>`);
+function saludar() {
+  //todo el codigo que sabe hacer la funcion
+  document.write(`<p>Hola Mundo</p>`);
 }
 
-function saludarNuevo(nombreSolicitado, apellidoSolicitado){
-    document.write(`<p>Hola ${nombreSolicitado}, ${apellidoSolicitado} 😊</p>`);
+function saludarNuevo(nombreSolicitado, apellidoSolicitado) {
+  document.write(`<p>Hola ${nombreSolicitado}, ${apellidoSolicitado} 😊</p>`);
 }
 
 //funcion que retorna un resultado
-function multiplicar(nro1, nro2){
-    const resultado = nro1 * nro2;
-    console.log(resultado);
-    return resultado;
-}
+// function multiplicar(nro1, nro2){
+//     const resultado = nro1 * nro2;
+//     console.log(resultado);
+//     return resultado;
+// }
 
+//arrow fn
+// const multiplicar = (nro1, nro2) => {
+//   const resultado = nro1 * nro2;
+//   console.log(resultado);
+//   return resultado;
+// };
+
+const multiplicar = (nro1, nro2) => nro1 * nro2;
+  
 //llamar o invocar a una funcion
 saludar();
 
-const nombre = prompt('Ingrese un nombre');
-const apellido = prompt('Ingrese un apellido');
+const nombre = prompt("Ingrese un nombre");
+const apellido = prompt("Ingrese un apellido");
 
 saludarNuevo(nombre, apellido);
-saludarNuevo('Jocker');
-saludarNuevo('Harry', 'Potter');
+saludarNuevo("Jocker");
+saludarNuevo("Harry", "Potter");
 
 const resultadoNuevo = multiplicar(30, 3);
-document.write(`<p>El resultado de la multiplicacion fue: ${resultadoNuevo}</p>`);
-document.write(`<p>El resultado de la multiplicacion fue: ${multiplicar(30, 3)}</p>`);
-
+document.write(
+  `<p>El resultado de la multiplicacion fue: ${resultadoNuevo}</p>`
+);
+document.write(
+  `<p>El resultado de la multiplicacion fue: ${multiplicar(30, 3)}</p>`
+);
